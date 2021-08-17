1a3ab2288e593b2a1b5fddec8b432d53b40ef74f
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-08-17 21:05:02 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-17082021
     Your branch is up to date with 'origin/taglio-17082021'.
    
     Changes to be committed:
    	new file:   config/scripts/ESAU_netwatch.sh
    	new file:   config/scripts/ESBG_netwatch.sh
    	new file:   config/scripts/ESDE_netwatch.sh
    	new file:   config/scripts/ESFR_netwatch.sh
    	new file:   config/scripts/ESJP_netwatch.sh
    	new file:   config/scripts/ESUK_netwatch.sh
    	new file:   config/scripts/ESUS_netwatch.sh

---
+++ config/scripts/ESDE_netwatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROUTER_IP=10.10.10.169
+IPSEC="telecomlobby-DE"
+GRE="tun5"
+
+PING_RESULT=$(/usr/bin/fping -I$GRE $ROUTER_IP 2>&1)
+ALIVE="alive"
+STATUS=$(/usr/sbin/ipsec status $IPSEC)
+ESTABLISHED="INSTALLED"
+
+if [[ "$PING_RESULT" != *"$ALIVE"* ]]; then
+	/bin/ps axu | grep "$IPSEC\|CRON\|netwatch" | grep -v grep | grep -v $$ | awk '{print $2}' | xargs kill -9 
+	/usr/sbin/ipsec stroke down-nb $IPSEC
+	/usr/sbin/ipsec down $IPSEC
+	/usr/sbin/ipsec up $IPSEC
+fi 
